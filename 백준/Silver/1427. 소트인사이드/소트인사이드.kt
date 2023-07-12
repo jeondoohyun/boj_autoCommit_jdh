7912fe4e0a1ecba4848176cd93a160c90ea6b428
dhjeon <67771076+jeondoohyun@users.noreply.github.com>
2023-07-12 15:54:07 +0900

    [Silver V] Title: 소트인사이드, Time: 144 ms, Memory: 23984 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.kt
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.kt
@@ -10,7 +10,8 @@ fun main() = with(System.out.bufferedWriter()) {
     N_string.forEach {
         list.add(it.toString())
     }
-    
+
+    // 아스키코드순으로 되어 있기 때문에 굳이 Int형으로 바꾼다음에 정렬할 필요 없이 스트링값으로 정렬시켜도 내림차순 적용가능
     val list_descending = list.sortedDescending()
 
     var sb = StringBuffer()
