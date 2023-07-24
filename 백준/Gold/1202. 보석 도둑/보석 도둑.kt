de73a1e1ab6217a3cc810c1007d1a54c7bc3239a
dhjeon <67771076+jeondoohyun@users.noreply.github.com>
2023-07-24 15:51:59 +0900

    [Gold II] Title: 보석 도둑, Time: 2036 ms, Memory: 162324 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.kt
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.kt
@@ -1,48 +1,53 @@
-import java.util.Collections
-import java.util.PriorityQueue
-
-data class Jewelry(val weight: Int, val price: Int) : Comparable<Jewelry> {
-    override fun compareTo(other: Jewelry): Int {
-        return if (this.weight != other.weight) this.weight - other.weight // 무게로 내림차순 정렬
-        else other.price - this.price // 무게가 같으면 가격으로 오름차순 정렬
-    }
-}
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
 fun main() {
-    val br = System.`in`.bufferedReader()
-    // n - 보석의 수, k - 가방의 수
-    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val firstInput = readLine()!!.split(" ")
+    val n = firstInput[0].toInt()
+    val k = firstInput[1].toInt()
+    val jewelries = ArrayList<Jewelry>()
+    val bags = ArrayList<Int>()
+    val pq = PriorityQueue(Comparator.reverseOrder<Int>())
+    var sum: Long = 0
+    var position = 0
 
-    val pqJewelries = PriorityQueue<Jewelry>()
-    val bag = Array(k) { 0 }
-
-    repeat(n) {
-        val (w, p) = br.readLine().split(" ").map { it.toInt() }
-        pqJewelries.add(Jewelry(w,p))
+    for (i in 0 until n) {
+        val info = readLine()!!.split(" ")
+        jewelries.add(Jewelry(info[0].toInt(), info[1].toInt()))
     }
-    repeat(k) {
-        bag[it] = br.readLine().toInt()
+
+    for (i in 0 until k) {
+        bags.add(readLine()!!.toInt())
     }
 
-    // 무게 기준으로 오름차순 정렬
-    bag.sort()
+    jewelries.sortWith(Comparator<Jewelry> { j1, j2 ->
+        if (j1.weight == j2.weight) {
+            j2.value - j1.value
+        } else j1.weight - j2.weight
+    })
 
-    // 가격을 넣을 우선순위 큐. 가격이 높은 순으로 정렬
-    val pqPrice = PriorityQueue<Int>(Collections.reverseOrder())
-    var total = 0L
+    bags.sort()
 
-    bag.forEach { bagWeight ->
-        while (pqJewelries.isNotEmpty()) {
-            if(pqJewelries.peek().weight <= bagWeight) {
-                pqPrice.add(pqJewelries.poll().price)
+    for(bag in bags) {
+
+        for (i in position until jewelries.size) {
+            if (jewelries[i].weight <= bag) {
+                pq.offer(jewelries[i].value)
+                position = i + 1
             } else {
                 break
             }
         }
 
-        // 가장 비싼 값 하나를 꺼냄 (배낭 하나당 한개의 보석만 들어갈 수 있기 때문에 하나만)
-        if (pqPrice.isNotEmpty()) total += pqPrice.poll()
+        if (!pq.isEmpty()) {
+            sum += pq.poll()
+        }
     }
-
-    println(total)
+    println(sum)
 }
+
+data class Jewelry(
+        val weight: Int,
+        val value: Int
+)
