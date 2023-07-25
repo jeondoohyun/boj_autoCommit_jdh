a0e8843d1b835c8c76ddd6307b865af8cbf2cecd
dhjeon <67771076+jeondoohyun@users.noreply.github.com>
2023-07-25 16:15:39 +0900

    [Gold II] Title: 보석 도둑, Time: 1900 ms, Memory: 193220 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.kt
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.kt
@@ -1,53 +1,77 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.util.*
 import kotlin.Comparator
-import kotlin.collections.ArrayList
 
-fun main() {
-    val firstInput = readLine()!!.split(" ")
-    val n = firstInput[0].toInt()
-    val k = firstInput[1].toInt()
-    val jewelries = ArrayList<Jewelry>()
-    val bags = ArrayList<Int>()
+data class Jewelry(var M: Int, var V: Int)
+
+fun main(args: Array<String>) {
+    // 우선순위 큐를 사용 하여 푼다
+    var br = BufferedReader(InputStreamReader(System.`in`))
+    var bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    var arr = mutableListOf<Jewelry>()
     val pq = PriorityQueue(Comparator.reverseOrder<Int>())
-    var sum: Long = 0
-    var position = 0
 
-    for (i in 0 until n) {
-        val info = readLine()!!.split(" ")
-        jewelries.add(Jewelry(info[0].toInt(), info[1].toInt()))
+    var N_K = br.readLine().split(" ").map { it.toInt() }
+
+    for (i in 0 until N_K[0]) {
+        var n_k = br.readLine().split(" ").map { it.toInt() }
+        arr.add(Jewelry(n_k[0], n_k[1]))
     }
 
-    for (i in 0 until k) {
-        bags.add(readLine()!!.toInt())
+    var C = mutableListOf<Int>()    // 배낭에 넣을수 있는 무게 한도
+    for (i in 0 until N_K[1]) {
+        C.add(br.readLine().toInt())
     }
 
-    jewelries.sortWith(Comparator<Jewelry> { j1, j2 ->
-        if (j1.weight == j2.weight) {
-            j2.value - j1.value
-        } else j1.weight - j2.weight
+    C.sort()
+
+    val arr_2 = arr.sortedWith(Comparator { j1, j2 ->
+//        when {
+//            t.M < t2.M -> -1
+//            t.M == t2.M -> when {
+//                t.V < t2.V -> 1
+//                else -> -1
+//            }
+//            else -> 1
+//        }
+        if (j1.M == j2.M) {
+            j2.V - j1.V
+        } else j1.M - j2.M
     })
 
-    bags.sort()
+    var result: Long = 0
+    var num = 0
 
-    for(bag in bags) {
-
-        for (i in position until jewelries.size) {
-            if (jewelries[i].weight <= bag) {
-                pq.offer(jewelries[i].value)
-                position = i + 1
-            } else {
-                break
+    C.forEach {a ->
+        run {
+//            arr_2.forEach {b ->
+//                if (a >= b.M) {
+//                    result += b.V
+//                    return@run
+//                } else return@run
+//            }
+            for (i in num until arr_2.size) {
+                if (a >= arr_2[num].M) {
+                    pq.offer(arr_2[i].V)
+                    num++
+                } else {
+                    return@run
+                }
             }
         }
 
         if (!pq.isEmpty()) {
-            sum += pq.poll()
+            result += pq.poll()
         }
     }
-    println(sum)
-}
 
-data class Jewelry(
-        val weight: Int,
-        val value: Int
-)
+    bw.write("$result")
+    bw.flush()
+    bw.close()
+
+
+}
