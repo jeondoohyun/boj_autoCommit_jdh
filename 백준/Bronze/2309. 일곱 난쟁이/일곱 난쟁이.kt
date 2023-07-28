3ce348dfffb0904c70d84219c81beadf094d8106
dhjeon <67771076+jeondoohyun@users.noreply.github.com>
2023-07-28 15:35:10 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.kt
@@ -0,0 +1,45 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.lang.Exception
+
+fun main(args: Array<String>) {
+    // 완전탐색(전부다 차례로 확인하는것)
+    // 데이터가 9가지 밖에 안주어지기 때문에 그냥 다 더해도 오래 걸리지 않는다. 7개를 더해서 100을 확인하던가 9개를 더한값에서 2개데이터를 빼서 100을 만들던가
+    try {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+        var 아홉난쟁이 = mutableListOf<Int>()
+
+        repeat(9) {
+            아홉난쟁이.add(br.readLine().toInt())
+        }
+
+        var sum = 아홉난쟁이.sum()
+        var flag = false
+
+        for (i in 0..7) {
+            for (j in i+1 .. 8) {
+                var result = sum - 아홉난쟁이[i] - 아홉난쟁이[j]  // 왜 IndexOutOfBound가 뜨지
+                if (result == 100) {
+                    아홉난쟁이.removeAt(i)   // 요소를 빼버려서 리스트 인덱스가 짧아졌는데 for문 계속 돌리면 indexOutOfBound
+                    아홉난쟁이.removeAt(j-1) // 
+                    아홉난쟁이.sort()
+                    flag = true
+                    break
+                }
+            }
+            if (flag) break
+        }
+
+        아홉난쟁이.forEach{
+            bw.write("$it\n")
+        }
+        bw.flush()
+        bw.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
