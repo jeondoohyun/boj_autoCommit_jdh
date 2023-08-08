db14a399cb22f4fa6c33d6ac92a89ca0240fc97c
dhjeon <67771076+jeondoohyun@users.noreply.github.com>
2023-08-08 18:13:18 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 132 ms, Memory: 20720 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.kt
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.kt
@@ -1,32 +1,52 @@
-import java.util.*
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.lang.Exception
 
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    val plugs = StringTokenizer(br.readLine()).run { IntArray(k) {nextToken().toInt()} }
-    val used = BooleanArray(k+1)
-    var result = 0
+lateinit var multitab: BooleanArray
+fun main(args: Array<String>) {
+    try {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    for ((index, plug) in plugs.withIndex()) {
-        if (used[plug]) continue // 이미 꽂혀있음
-        if (used.count{it} < n) used[plug] = true // 비어 있는 곳이 있다면 넣어준다.
-        else {
-            val list = mutableListOf<Int>()
-            for (next in index + 1 until k) {
-                if (used[plugs[next]] && plugs[next] !in list) list.add(plugs[next])
-            }
+        var (n,k) = br.readLine().split(" ").map { it.toInt() }
+        var products = br.readLine().split(" ").map { it.toInt() }
 
-            if (list.size != n) {
-                // 나중에 사용되지 않을 콘센트를 제거해준다.
-                for ((nextPlug, use) in used.withIndex()) {
-                    if (use && nextPlug !in list) {used[nextPlug] = false; break}
+        multitab = BooleanArray(k+1)
+
+        var cnt = 0
+
+        for (i in 0 until products.size) {
+            if (multitab[products[i]]) continue // 3구 멀티탭에 1212 사용 순서일때 세번째 1은 이미 꽂혀있기때문에 continue로 다음으로 넘어감
+            if (multitab.count { it } < n) {
+                multitab[products[i]] = true
+            } else {  // 3구 멀티탭에 이미 3개가 꽂혀있어서 뽑을 기기를 정해야함
+                var reuse_arr = mutableListOf<Int>()
+                for (next in i+1 until products.size) {
+                    // 재사용해야 하는 기기 저장, 재사용하지 않는 기기들을 멀티탭에서 빼준다
+                    if (multitab[products[next]] && products[next] !in reuse_arr) reuse_arr.add(products[next]) // reuse에 중복되면 추가를 안하기 때문에 reuse가 n값보다 커질수가 없다
                 }
-            } else used[list.last()] = false // 가장 마지막에 사용될 콘센트 제거
 
-            used[plug] = true
-            result += 1
+                if (reuse_arr.size != n) {
+                    for (del in 0 until products.size) {
+                        if (multitab[del] && del !in reuse_arr) { multitab[del] = false;break }
+                    }
+                } else { // 현재 멀티탭에 꽂혀있는 기기 모두 나중에 재사용 된다. 제일 나중에 사용되는 기기를 빼준다.
+                    multitab[reuse_arr.last()] = false
+                }
+                multitab[products[i]] = true
+                cnt++
+            }
         }
-    }
 
-    print(result)
+        bw.write("$cnt")
+        bw.flush()
+        bw.close()
+
+
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
